ba5e1e62ef94db711315beefd33e5059947ec37c
stu-keil <stephanekeil@gmail.com>
2016-05-05 17:20:29 -0500

    Añadi metodo de producto

--- Composicion/medidas_composicion.py
+++ Composicion/medidas_composicion.py
@@ -3,6 +3,13 @@
 Created on Thu May  5 10:59:19 2016
 
 @author: stuka
+
+Este programa toma un corpus:
+
+1- Lo limpia
+2- Genera una representación vectorial de todos sus token
+3- Calcula de diferentes maneras la composición de palabras de cada documento para tener una representación vectorial a nivel documento
+4- Utiliza un algoritmo de agrupamiento para generar conglomerados de documentos
 """
 
 import numpy as np
@@ -22,12 +29,25 @@ import gensim
 
 
 def plot_words(Z,ids,mark='o',color='blue'):
+    """
+Este metodo permite mostrar la representación vectorial del corpus estudiado despues de aplicar word2vec    
+    """
 	r=0
 	plt.scatter(Z[:,0],Z[:,1], marker=mark, c=color)
 	for label,x,y in zip(ids, Z[:,0], Z[:,1]):
 		plt.annotate(label.decode('utf8'), xy=(x,y), xytext=(-1,1), textcoords='offset points', ha= 'center', va='bottom', bbox=dict(boxstyle='round,pad=0.5', fc='white', alpha=0.0), arrowprops=dict(arrowstyle='-', connectionstyle='arc3,rad=0'))
 		r+=1
 
+def generaClusters(matriz,numclust):
+    """
+    Este metodo permite generar los conglomerados de los documentos que se quieren representar, es necesario proveer una matriz de n = documentos x m = numero de dimensiones sobre la que se realiza la representación vectorial.
+    """
+    #### Clustering
+    nclusters = 21
+    modelo_cluster = KMeans(n_clusters=nclusters)
+    modelo_cluster.fit(matriz)
+    labels = modelo_cluster.labels_
+    return labels
 
 os.getcwd()
 os.chdir("/home/stuka/itam2/textmining/text-mining/Composicion")
@@ -78,14 +98,25 @@ for line in range(len(sentences)):
             rep_vect_doc_sum[line][coord] += rep_vect.loc[sentences[line][word]][coord]
  
 plot_words(rep_vect_doc_sum,list(corpus['oracion']))
+labels = generaClusters(rep_vect_doc_sum,21)
 
-corpus['clave'].value_counts()
-corpus['clave'].value_counts().count()
-
-#### Clustering
-nclusters = 21
-modelo_cluster = KMeans(n_clusters=nclusters)
-modelo_cluster.fit(rep_vect_doc_sum)
-labels = modelo_cluster.labels_
 eval_final = pd.concat([corpus,pd.DataFrame(labels),pd.DataFrame(rep_vect_doc_sum)],axis=1)
-eval_final.to_csv('modelo_final.csv',encoding='utf-8',index=False)
+eval_final.to_csv('modelo_final_sum.csv',encoding='utf-8',index=False)
+
+##### Funcion Producto
+rep_vect_doc_prod = np.full((len(corpus.index),len(rep_vect.columns)),1.0, dtype=np.double)
+for line in range(len(sentences)):
+    for word in range(len(sentences[line])):
+        for coord in range(len(rep_vect.columns)):
+            rep_vect_doc_prod[line][coord] *= rep_vect.loc[sentences[line][word]][coord]
+plot_words(rep_vect_doc_prod,list(corpus['oracion']))
+labels = generaClusters(rep_vect_doc_prod,21)
+
+eval_final = pd.concat([corpus,pd.DataFrame(labels),pd.DataFrame(rep_vect_doc_prod)],axis=1)
+eval_final.to_csv('modelo_final_prod.csv',encoding='utf-8',index=False)
+
+
+
+
+    
+
